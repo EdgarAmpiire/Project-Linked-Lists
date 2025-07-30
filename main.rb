a649b603fb02c1853891066c75038b64aa53708e
Edgar Ampiire <eampiire@gmail.com.com>
2025-07-31 00:27:44 +0300

    main.rb logic to test out linked lists

---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative 'linked_list'
+
+list = LinkedList.new
+list.append('cat')
+list.append('lion')
+list.append('cheater')
+list.append('tiger')
+list.append('leopard')
+list.append('jaguar')
+list.append('panther')
+
+puts list
